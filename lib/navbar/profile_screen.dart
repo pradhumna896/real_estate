773eb15937d1335a8b634b0aca88c39df5b051f4
pradhumna mirotha <pradhumna896@gmail.com>
2023-01-16 13:08:53 +0530

    add

--- lib/navbar/profile_screen.dart
+++ lib/navbar/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:real_estate/widget/appText.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -11,6 +12,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Stack(
       children: [
         Positioned(
@@ -27,12 +29,42 @@ class _ProfileScreenState extends State<ProfileScreen> {
           left: 0, 
           right: 0, 
           bottom: 0,
-          top: height*0.35,
+          top: height*0.3,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            decoration: const  BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(25),topRight: Radius.circular(25)),
+              color:Colors.white),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 SizedBox(height: height*0.1,),
+                 AppText(text: "Name", color: Colors.black, fontWeight: FontWeight.bold, size: 28)
+              ],),
+          )),
+        Positioned(
+          top: height*0.2,
+          left: (width-150)*0.5,
+          right: (width-150)*0.5,
+          height: height*0.2,
           child: Container(
             decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(topRight: Radius.circular(10)),
-              color:Colors.red),
-          ))
+              shape: BoxShape.circle,
+              image: DecorationImage(image: AssetImage("image/home.jpg"),fit: BoxFit.cover)
+            ),
+          )),  
+        Positioned(
+          right: 10, 
+          bottom: 10,
+          left: 10,
+          height: height*0.1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.amberAccent,
+              borderRadius: BorderRadius.circular(12)),
+          ),
+          
+          )
       ],
     );
   }
--- lib/navbar/search_screen.dart
+++ lib/navbar/search_screen.dart
@@ -1,15 +1,56 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-class DiscoverScreen extends StatefulWidget {
+class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
-  State<DiscoverScreen> createState() => _DiscoverScreenState();
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Opacity(opacity: 0.5,
+         child: ClipPath(
+          clipper: WaveClipper(),
+          child: Container(
+          height: 300,
+          color: Colors.deepOrange,)),
+        ), 
+        ClipPath(
+          clipper: WaveClipper(),
+          child: Container(
+          height: 300,
+          decoration:const BoxDecoration(
+            image: DecorationImage(image: AssetImage("image/home.jpg"),fit: BoxFit.cover)
+          ),)),
+      ],
+    );
+  }
 }
 
-class _DiscoverScreenState extends State<DiscoverScreen> {
+class WaveClipper extends CustomClipper<Path>{
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text("Discover Location"));
+  Path getClip(Size size){
+    var path = Path();
+    path.lineTo(0, size.height);
+
+    var firstStart = Offset(size.width/5, size.height);
+    var firstEnd = Offset(size.width/2.25, size.height-50);
+    path.quadraticBezierTo(firstStart.dx, firstStart.dy, firstEnd.dx  ,firstEnd.dy);
+    var secondStart = Offset(size.width-(size.width/3.24), size.height-105);
+    var secondEnd = Offset(size.width, size.height-10);
+
+    path.quadraticBezierTo(secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
   }
+  
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    throw UnimplementedError();
+  }
+  
+ 
+  
 }
